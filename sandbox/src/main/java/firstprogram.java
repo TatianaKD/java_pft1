4de79ffc1a4dc101a6f0520742c08c086d2f28d1
TatianaKD <93261387+TatianaKD@users.noreply.github.com>
2021-10-29 16:04:13 +0400

    Создала класс Point,в котором описала атрибуты точки на плоскости (координаты x и y), создала конструктор(создает объект класса Point, конкретную точку), создала метод вычисления раастояния между точками на плоскости, вызвала метод через точку входа( функцию main) в классе app.

--- sandbox/src/main/java/firstprogram.java
+++ sandbox/src/main/java/firstprogram.java
@@ -1,5 +1,10 @@
 class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Point p1 = new Point(2.1, 3.2);
+        Point p2 = new Point(4.2, 3.4);
+
+
+        System.out.print(Point.distance(p1, p2));
     }
 }
