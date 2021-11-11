17b4dbcec95241c1a898f7599f240d33db7521ca
TatianaKD <93261387+TatianaKD@users.noreply.github.com>
2021-11-11 15:14:33 +0400

    заменила наследование на делегирование и создала двухуровневую архитектуру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,11 +1,11 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
     private String name;
     private String header;
     private String footer;
 
-    public GroupData(String name,String header,String footer){
+    public GroupData(String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
